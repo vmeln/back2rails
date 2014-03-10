237020aaba6b5ed5c4c39224e380e3f3258b03f0
shox <shox@debian.(none)>
2014-03-10 22:01:50 +0200

    Added several unit-tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ActiveRecord::Base
+  validates :title, :description, :image_url, :presence => true
+  validates :price, :numericality => { :greater_than_or_equal_to => 0.01 }
+  validates :title, :uniqueness => true
+
+  # TODO: add Image Url validation after image uploading
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,36 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :products
+
+  test 'Empty product should be invalid' do
+    product = Product.new
+    assert(product.invalid?, 'Product should be invalid')
+    assert(product.errors[:title].any?, 'Title should be invalid')
+    assert(product.errors[:description].any?, 'Description should be present')
+    assert(product.errors[:price].any?, 'Price should be present')
+  end
+
+  test 'Product price should be positive' do
+    product = Product.new(:title => 'Product', :description => 'Description', :image_url => 'some.url.gif')
+    assert(product.invalid?, 'Product should be invalid due to unset price')
+
+    product.price = -100
+    assert(product.invalid?, 'Product should be invalid with negative price')
+    product.price = 100
+    assert(product.price, 'Now the product should be valid')
+  end
+
+  test 'Name should be unique' do
+    product = Product.new(
+        :title => products(:first).title,
+        :description => 'Super description',
+        :image_url => 'url.gif',
+        :price => 20.0)
+
+    assert !product.save
+
+    # Can't find in translations
+    assert_equal('has already been taken', product.errors[:title].join('; '))
+  end
 end
